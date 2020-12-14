da00f55c1692d273af6e0bf2172e767cd108f4eb
lordfarron <brandon.brunno@gmail.com>
2020-12-14 14:06:35 -0300

    Add Visualization by average of grades per year

--- src/app.js
+++ src/app.js
@@ -9,46 +9,64 @@ const getData = async () => {
 
     var i = 0;
     var counter = 0;
+    contarDenominador = 0;
 
      const table = data.split('\n').slice(1);
      table.forEach((row, index) => {
         const columns = row.split(',');
         const year = columns[0];
-        xlabels.push(year);
+        if (year == "end"){
+            xlabels.push(year);
+            xlabels.pop(year)
+        } else {
+            xlabels.push(year);
+        }
+        
         const sgUF = columns[2];
         const numMunicipio = columns[4];
         const nomeEscola = columns[6]; //NO_ESCOLA_EDUCACENSO
         labelEscola.push(nomeEscola);
         const numTaxaAprova = columns[23]; // NU_TAXA_APROV
         ytaxaAprov.push(numTaxaAprova);
+        //console.log("Taxa Aprov " + ytaxaAprov)
         const numTaxaReprov = columns[24]; // NU_TAXA_REPROV
         const numTaxaAbando = columns[25]; // NU_TAXA_ANADONO
         /*console.log(year, sgUF, numMunicipio,
         nomeEscola, numTaxaAprova, numTaxaReprov, numTaxaAbando);*/
-      
+
+
+
         console.log(xlabels[counter] + " and " + xlabels[counter - 1])
-        if(xlabels[counter] == xlabels[counter - 1]){
+        if(xlabels[counter] == xlabels[counter - 1]){ //caso o label atual seja igual ao anterior (mesmo ano)
             //console.log("Before sum " + ytaxaAprovUnique[i])
-            ytaxaAprovUnique[i] += parseInt(ytaxaAprov[counter]);
+            contarDenominador++;
+            ytaxaAprovUnique[i] += parseFloat(ytaxaAprov[counter]);
             console.log("After SUM " + ytaxaAprovUnique[i])
             //console.log(ytaxaAprovUnique);
-        } else {
+        } else if(xlabels[counter - 1] == undefined){ // caso label anterior seja undefined (começo do array)
+            contarDenominador++;
+            console.log("Time runned")
+            ytaxaAprovUnique[i] = parseFloat(ytaxaAprov[counter]);
+            console.log("After SUM " + ytaxaAprovUnique[i])
+            //console.log("Undefined Value? " + ytaxaAprovUnique[i])
+        } else if (xlabels[counter] != xlabels[counter - 1]){ // caso o label atual seja diferente do label anterior (inicialia o proximo array)
+            ytaxaAprovDivided[i] = Math.floor(ytaxaAprovUnique[i]/contarDenominador);
+            console.log("YTAXA no valor", ytaxaAprovUnique[i],
+            "dividido por",contarDenominador, "resulta em", ytaxaAprovDivided[i])
             i++;
-            ytaxaAprovUnique[i] += ytaxaAprov[i];
+            contarDenominador = 0;
+            ytaxaAprovUnique[i] = parseFloat(ytaxaAprov[counter]);
+            contarDenominador++;
+            console.log("After SUM " + ytaxaAprovUnique[i])
+            
         }
+        //console.log("Valor Counter " + ytaxaAprov[counter])
         counter++;
      })
-
+        
         labelEscolaUnique = labelEscola.filter((value, index, self) => {
             return self.indexOf(value) === index;
         })
-
-        /*xlabelsUnique = xlabels.filter((value, index, self) => {
-            if(self.indexOf(value) === index){
-                return ytaxaAprovUnique[index] += ytaxaAprov[index]/ labelEscolaUnique.length + 1
-            }
-        //return self.indexOf(value) === index;
-    });*/
 
     xlabelsUnique = xlabels.filter((value, index, self) =>{
         return self.indexOf(value) === index;
@@ -67,6 +85,8 @@ var xlabelsUnique = [];
 var xlabelsOrdered = [];
 
 var ytaxaAprovUnique = [];
+var ytaxaAprovDivided = [];
+var contarDenominador;
 
 var labelEscola = [];
 var labelEscolaUnique = [];
@@ -77,6 +97,8 @@ let label = "Aprovador por ano";
 
 const drawChart = async () => {
     await getData();
+   
+    //console.log(typeof(parseInt(ytaxaAprovUnique)))
     
     var ctx = document.getElementById('chart');
 
@@ -86,7 +108,7 @@ const drawChart = async () => {
             labels: xlabelsOrdered,
             datasets: [{
                 label: `${label}`,
-                data: ytaxaAprov,
+                data: ytaxaAprovDivided,
                 backgroundColor: 
                     'rgba(137, 221, 151, 0.7)',
                 borderColor: 
